1f0487137e92fc68992f6cfd8984915ed9e7bc19
HilmiAnugrah <hilmianugrah2003@gmail.com>
2023-06-08 20:31:49 +0700

    mengubah file functions

--- view/login.view.php
+++ view/login.view.php
@@ -1,26 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
-    <title>Hayde news | Login</title>
-=======
->>>>>>> a46c62358430cc40438d9af859bd6d4cb9b12d90
-    <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="logreg.css">
     <link rel="stylesheet" href="css/cssreset.css">
     <link rel="stylesheet" href="css/mybody.css">
-<<<<<<< HEAD
-    <?php require('partials/mycss.php'); ?>
-<body>
-    <?php require "partials/login.php"; ?>
-</body>
-</html>
-=======
     <?php require('partials/head.php'); ?>
     <title>Hayde news | Login</title>
 </head>
 <body>
     <?php require "partials/login.php"; ?>
 </body>
->>>>>>> a46c62358430cc40438d9af859bd6d4cb9b12d90
 </html>
--- view/register.view.php
+++ view/register.view.php
@@ -1,24 +1,13 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-<title>Hayde news | Register</title>
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="css/mybody.css">
-    <link rel="stylesheet" href="logreg.css">
-    <link rel="stylesheet" href="css/cssreset.css">
-    <?php require('partials/mycss.php'); ?>
-    
-=======
 <head>
 <title>Hayde news | Register</title>
-    <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="logreg.css">
     <link rel="stylesheet" href="css/mybody.css">
     <link rel="stylesheet" href="css/cssreset.css">
     <?php require('partials/head.php'); ?>
 </head>
->>>>>>> a46c62358430cc40438d9af859bd6d4cb9b12d90
 <body>
     <?php 
 require "partials/register.php";
